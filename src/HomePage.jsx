049adae393e652dfabb6ac41500a812009c23324
itsnotxarwa <marwa.kchkh@gmail.com>
2025-12-17 14:43:09 +0100

    update: add cas d'usage section

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,5 +1,8 @@
+import { Fade, Slide } from "react-awesome-reveal";
 import Header from "./Header";
-import HeroSection from "./HomeComponents/heroSection"
+import BenefitsSection from "./HomeComponents/BenefitsSection";
+import HeroSection from "./HomeComponents/heroSection";
+import UsageExamples from "./HomeComponents/UsageExamples";
 
 
 
@@ -7,13 +10,12 @@ export default function HomePage() {
 
     return(
         <div className="min-h-screen bg-black/90 text-white overflow-hidden">
-        {/* <div className="fixed inset-0 overflow-hidden pointer-events-none">
-                <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-[#F2C14E]/20 rounded-full blur-3xl animate-pulse"></div>
-                <div className="absolute bottom-1/4 right-1/4 w-96 h-96 bg-[#4EE2EC]/20 rounded-full blur-3xl animate-pulse delay-1000"></div>
-                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[800px] h-[800px] bg-gradient-radial from-[#6967FB]/10 via-transparent to-transparent rounded-full"></div>
-            </div> */}
             <Header />
             <HeroSection />
+            <Slide triggerOnce direction="up">
+            <BenefitsSection />
+            </Slide>
+            <UsageExamples />
         </div>
     )
 }
