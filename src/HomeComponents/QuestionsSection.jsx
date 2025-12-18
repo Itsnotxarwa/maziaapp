b0919f4ba3b2a1a77eb1c0cdf14817c63cf32815
itsnotxarwa <marwa.kchkh@gmail.com>
2025-12-18 13:06:23 +0100

    update FAQ section

--- src/HomeComponents/QuestionsSection.jsx
+++ src/HomeComponents/QuestionsSection.jsx
@@ -12,7 +12,7 @@ export default function QuestionsSection() {
 
     return(
         <Fade triggerOnce direction="up" duration={800}>
-        <section className="relative py-24 bg-black/95 mx-2 sm:mx-4 rounded-2xl">
+        <section className="relative py-24 sm:mx-4">
                 <div className="max-w-5xl mx-auto px-4 sm:px-6 relative">
                     <div className="text-center space-y-4 mb-10">
                         <h3 className="text-3xl sm:text-4xl font-bold tracking-tight text-white mb-1">FAQ.</h3>
@@ -23,21 +23,24 @@ export default function QuestionsSection() {
                         return(
                         <div 
                         key={i}
-                        className="backdrop-blur-sm border border-white/10 rounded-lg 
-                        overflow-hidden shadow-sm shadow-white/5 hover:border-white/15 transition-colors">
+                        className="flex flex-col group backdrop-blur-sm border-t border-white/10 rounded-lg 
+                        overflow-hidden hover:border-white/15 transition-colors">
                             <button 
                             className="w-full flex items-center justify-between px-5 py-4 text-left"
                             onClick={() => toggle(i)}>
-                                <h3 className="text-white font-medium md:text-lg text-base">{item.question}</h3>
+                                <div className="flex items-center gap-6">
+                                    <span className="text-sm text-gray-50/50 group-hover:text-gray-50">(00{item.id})</span>
+                                    <h3 className="text-white font-medium md:text-lg text-base">{item.question}</h3>
+                                </div>
                                 <div className="shrink-0 flex items-center justify-center h-6 w-6 rounded-full 
                                 bg-white/5 border border-white/10 text-white">
                                     {openIndex === i ? <Minus size={12} /> : <Plus size={12} />}
                                 </div>
                             </button>
                             {openIndex === i &&(
-                            <div className="px-5 pb-4">
+                            <div className="px-5 pb-4 w-full">
                                 <div className="w-10 h-px border-t border-dashed border-white/10 mb-3"></div>
-                                <p className="text-white/70 text-sm md:text-lg">{item.answer}</p>
+                                <p className="text-white/70 text-sm md:text-lg text-left">{item.answer}</p>
                             </div>
                             )}
                         </div>
---
+++ src/data/Questions.js
@@ -0,0 +1,17 @@
+export const question =[
+    {
+        id: 1,
+        question: "Comment MAZ gère-t-il les appels complexes ?",
+        answer: "L’IA détecte les scénarios complexes et peut transférer l’appel à un humain."
+    },
+    {
+        id: 2,
+        question: "Puis-je personnaliser les scripts d’appel ?",
+        answer: "Oui, les scripts sont entièrement modulables selon vos besoins."
+    },
+    {
+        id: 3,
+        question: "Les appels sont-ils sécurisés ?",
+        answer: "Toutes les données sont cryptées et conformes au RGPD."
+    }
+]
