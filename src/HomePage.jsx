ed6ebb81cde53f93fafe1e6dea277b0546bbd2f1
itsnotxarwa <marwa.kchkh@gmail.com>
2025-12-18 11:54:25 +0100

    add Footer section to landing page

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
 import { Slide } from "react-awesome-reveal";
-=======
-import { Fade, Slide } from "react-awesome-reveal";
->>>>>>> ab6a4755b3fe39da22eaf68b273e5d2499ea2fac
 import Header from "./Header";
 import BenefitsSection from "./HomeComponents/BenefitsSection";
 import HeroSection from "./HomeComponents/heroSection";
 import UsageExamples from "./HomeComponents/UsageExamples";
-<<<<<<< HEAD
 import Footer from "./Footer";
-=======
->>>>>>> ab6a4755b3fe39da22eaf68b273e5d2499ea2fac
 
 
 
@@ -24,10 +17,7 @@ export default function HomePage() {
             <BenefitsSection />
             </Slide>
             <UsageExamples />
-<<<<<<< HEAD
             <Footer />
-=======
->>>>>>> ab6a4755b3fe39da22eaf68b273e5d2499ea2fac
         </div>
     )
 }
