1842dc7ec02f690e0ad68c929513e9021285973b
itsnotxarwa <marwa.kchkh@gmail.com>
2025-12-19 15:12:18 +0100

    add Login page and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import HomePage from './HomePage';
-
+import Login from "./Login";
 function App() {
 
   return (
@@ -9,6 +9,7 @@ function App() {
       <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/connecter" element={<Login />} />
       </Routes>
     </Router>
     </div>
