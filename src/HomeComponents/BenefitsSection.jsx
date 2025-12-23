179955f3c6f4b09360c51f5c6f8a5908ddbf44cc
itsnotxarwa <marwa.kchkh@gmail.com>
2025-12-23 15:43:28 +0100

    update benefits data and benefits section

--- src/HomeComponents/BenefitsSection.jsx
+++ src/HomeComponents/BenefitsSection.jsx
@@ -1,83 +1,26 @@
-import { useState, useEffect } from "react";
-import { ChevronRight, ChevronLeft } from "lucide-react";
 import { benefits } from "../data/BenefitsData";
 import { Slide } from "react-awesome-reveal";
 import GradientIcon from "../components/GradientIcon";
 
 export default function BenefitsSection() {
-    
-    const [startIndex, setStartIndex] = useState(0);
-
-    const [cardsPerSlide, setCardsPerSlide] = useState(2);
-
-    useEffect(() => {
-      const handleResize = () => {
-        if (window.innerWidth >= 768) {
-          setCardsPerSlide(2);   
-        } else {
-          setCardsPerSlide(1);  
-        }
-      };
-    handleResize();
-    window.addEventListener("resize", handleResize);
-
-    return () => window.removeEventListener("resize", handleResize);
-    });
-
-    const totalSlides = Math.ceil(benefits.length / cardsPerSlide);
-
-    const nextSlide = () => {
-    setStartIndex((prev) =>
-        prev + cardsPerSlide >= benefits.length ? 0 : prev + cardsPerSlide
-    );
-    };
-
-    const prevSlide = () => {
-        setStartIndex((prev) =>
-        prev - cardsPerSlide < 0
-            ? (totalSlides - 1) * cardsPerSlide
-            : prev - cardsPerSlide
-        );
-    };
-
-    const visibleCards = benefits.slice(startIndex, startIndex + cardsPerSlide);
 
     return(
         <section className="relative pt-24 pb-32">
-          <Slide triggerOnce cascade direction="right" duration={1000}>
-            <div className="mb-6 hidden lg:flex justify-end gap-2  pr-8 sm:pr-6 lg:pr-16">
-            <button
-              onClick={prevSlide}
-              className="p-2 bg-white text-gray-300 border border-gray-300 rounded-lg hover:scale-105 transition-transform 
-              duration-300 hover:text-black"
-            >
-              <ChevronLeft />
-            </button>
-            <button
-              onClick={nextSlide}
-              className="p-2 bg-white text-gray-300 border border-gray-300 rounded-lg hover:scale-105 transition-transform 
-              duration-300 hover:text-black"
-            >
-            <ChevronRight />
-            </button>
-          </div>
-          </Slide>
-            <div className="max-w-7xl mx-auto px-8 sm:px-6 lg:px-16 flex flex-col lg:flex-row gap-8">
-              <Slide triggerOnce cascade direction="left" duration={1000}>
-                <div className="flex flex-col lg:w-[350px]">
-                    <h2 className="text-xl uppercase text-gray-50/50 mb-4">Pourquoi choisir Mazia ?</h2>
+            <div className="max-w-7xl mx-auto px-8 sm:px-6 lg:px-16">
+                <div className="flex items-center justify-center mb-20">
+                  <div className="space-y-4">
+                    <h2 className="text-lg text-center uppercase text-white/60 mb-4">Pourquoi choisir Mazia ?</h2>
                     <h2 className="text-2xl text-white font-medium mb-8 text-center">
                         Les bénéfices essentiels de Mazia pour transformer votre entreprise
                     </h2>
-                    <p className="text-xl text-left text-gray-50/50">
+                    <p className="text-xl mb-6 text-center text-gray-50/50">
                     Mazia n’est pas seulement un outil d’automatisation : c’est un véritable levier stratégique qui révolutionne la gestion des appels commerciaux et l’expérience client.
                     </p>
+                  </div>
                 </div>
-                </Slide>
-                <Slide triggerOnce cascade direction="right" duration={1000}>
                 <div className="flex-1">
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            {visibleCards.map((item, i) => {
+                <div className="grid grid-cols-1 md:grid-cols-4 gap-2">
+            {benefits.map((item, i) => {
               const Icon = item.icon;
               return (
                 <div
@@ -96,24 +39,7 @@ export default function BenefitsSection() {
             })}
           </div>
           </div>
-          </Slide>
         </div>
-         <div className="mt-8 lg:hidden mb-6 flex justify-center gap-2  pr-8 sm:pr-6 lg:pr-16">
-            <button
-              onClick={prevSlide}
-              className="p-2 bg-white text-gray-300 border border-gray-300 rounded-lg hover:scale-105 transition-transform 
-                        duration-300 hover:text-black"
-            >
-              <ChevronLeft />
-            </button>
-            <button
-              onClick={nextSlide}
-              className="p-2 bg-white text-gray-300 border border-gray-300 rounded-lg hover:scale-105 transition-transform 
-                                                duration-300 hover:text-black"
-            >
-            <ChevronRight />
-            </button>
-          </div>
-        </section>
+          </section>
     )
 }
--- src/data/BenefitsData.js
+++ src/data/BenefitsData.js
@@ -4,25 +4,25 @@ export const benefits = [
   {
     title: "Gain de temps",
     description:
-      "Voixup prend en charge toutes les tâches répétitives et chronophages, permettant à vos équipes de se concentrer sur les missions à forte valeur ajoutée.",
+      "Mazia automatise les tâches répétitives pour libérer du temps à vos équipes.",
     icon: Clock,
   },
   {
     title: "Efficacité",
     description:
-      "Les appels sont traités rapidement, sans erreurs et avec une précision constante, améliorant la qualité globale de vos opérations.",
+      "Des appels traités rapidement, sans erreurs et avec une précision constante.",
     icon: Zap,
   },
   {
     title: "Réduction des coûts",
     description:
-      "Économisez sur les salaires et charges sociales tout en améliorant l'efficacité de votre accueil téléphonique.",
+      "Réduisez les coûts tout en optimisant l’efficacité de votre accueil téléphonique.",
     icon: TrendingUp,
   },
   {
     title: "Satisfaction client",
     description:
-      "Une présence professionnelle 24/7, qui garantit une expérience fluide, chaleureuse et fiable à chaque interaction.",
+      "Une présence professionnelle 24/7 pour une expérience fluide et fiable.",
     icon: HandHeart,
   },
 ];
